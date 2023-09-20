95803538b022658ed0462c625a4722f22180135f
sheyla.yaker <sheyla.yaker@enarahealth.com>
2023-09-19 21:15:59 -0500

    add classes to support menu logic, include main inicialization

--- src/main/java/upc/trabajo_final/InterfazSistema.java
+++ src/main/java/upc/trabajo_final/InterfazSistema.java
@@ -52,7 +52,6 @@ public class InterfazSistema implements IInterfazSistema {
         if (menu == null) {
             this.menuActual = menuIdioma;
         }
-
         menuActual.imprimirMenu();
     }
 
